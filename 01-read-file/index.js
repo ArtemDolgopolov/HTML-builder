f45b7997f5f8e6b117d020826addb905aba1f503
ArtemDolgopolov <artem.dolgopolov94@gmail.com>
2023-05-04 00:32:18 +0500

    feat: did 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathToHTML = path.join(__dirname, 'text.txt');
+
+fs.readFile(pathToHTML, 'utf-8', (error, data) => {
+ if (error) throw error;
+ else console.log(data);
+})
