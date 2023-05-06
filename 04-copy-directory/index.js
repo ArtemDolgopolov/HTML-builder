0b9ed6263fcb03b777ce05918b9b8918859e2b39
ArtemDolgopolov <artem.dolgopolov94@gmail.com>
2023-05-06 19:04:44 +0500

    feat: did 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require('fs').promises;
+const path = require('path');
 
+async function copyDir() {
+  const destPath = path.join(__dirname, 'files-copy');
+  await fs.mkdir(destPath, {recursive: true});
+
+  const srcPath = './04-copy-directory/files';
+  const files = await fs.readdir(srcPath);
+
+  for (const file of files) {
+    const pathToSrcFile = path.join(srcPath, file);
+    const pathToDestFile = path.join(destPath, file);
+    await fs.copyFile(pathToSrcFile, pathToDestFile);
+  }
+
+  console.log('Copying is over');
+}
+
+copyDir().catch(err => console.error(err));
