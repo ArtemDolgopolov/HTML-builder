04951ecf0e96b2ffa432cef2970ab79f7a1a19aa
ArtemDolgopolov <artem.dolgopolov94@gmail.com>
2023-05-08 14:42:30 +0500

    fix: fix eslint errors in 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -4,6 +4,6 @@ const path = require('path');
 const pathToHTML = path.join(__dirname, 'text.txt');
 
 fs.readFile(pathToHTML, 'utf-8', (error, data) => {
- if (error) throw error;
- else console.log(data);
-})
+  if (error) throw error;
+  else console.log(data);
+});
