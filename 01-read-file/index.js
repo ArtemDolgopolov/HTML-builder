d8ab3c960e5f9a6db0c6633c08f75b03cefac7f9
ArtemDolgopolov <artem.dolgopolov94@gmail.com>
2023-05-11 23:00:25 +0500

    refactor: fix 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,7 +3,8 @@ const path = require('path');
 
 const pathToHTML = path.join(__dirname, 'text.txt');
 
-fs.readFile(pathToHTML, 'utf-8', (error, data) => {
-  if (error) throw error;
-  else console.log(data);
+const readStream = fs.createReadStream(pathToHTML, 'utf-8');
+
+readStream.on('data', (data) => {
+  console.log(data);
 });
